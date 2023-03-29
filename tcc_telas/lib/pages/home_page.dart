80ebd1a291a0e18300807edc7e21b316f353b6e3
chaba2005 <rtromero.sp@gmail.com>
2023-03-29 14:49:50 -0300

    Alterações na home page

--- tcc_telas/lib/pages/home_page.dart
+++ tcc_telas/lib/pages/home_page.dart
@@ -3,24 +3,52 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget{
+class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFA12E2F),
-        leading:IconButton(
-            icon: Image.asset('image/logo.png'),
-            onPressed: (() => homePage()),
+        leading: IconButton(
+          icon: Image.asset('image/logo.png'),
+          onPressed: (() => homePage()),
+        ),
+        leadingWidth: 80,
+        title: Text(
+          'SARsCamp',
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
+        ),
+        titleSpacing: 0,
+        actions: [
+          SizedBox(
+            width: 50, // largura do espaço em pixels
+            child: IconButton(
+              icon: Icon(
+                Icons.star,
+                size: 40,
+              ),
+              onPressed: () {
+                // função chamada quando o ícone de estrela é pressionado
+              },
+            ),
           ),
-          leadingWidth: 100,
-          title: Text('SARsCamp'), 
+          SizedBox(
+            width: 50, // largura do espaço em pixels
+            child: IconButton(
+              icon: Icon(
+                Icons.info,
+                size: 40,
+              ),
+              onPressed: () {
+                // função chamada quando o ícone de informações é pressionado
+              },
+            ),
+          ),
+        ],
       ),
       body: Container(
         color: Color(0xFF0A6066),
-        child: Center(
-          child: Image.asset('image/Unicamp')
-        ),
+        child: Center(child: Image.asset('image/Unicamp.jpg')),
       ),
     );
   }

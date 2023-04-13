08475fc02d9b30905f2b9ca7939d79c398518ea5
Chaba <rtromer.sp@gmail.com>
2023-04-12 22:08:29 -0300

    ebaaa, ta tudo no mesmo

--- tcc_telas/lib/pages/saldo_page.dart
+++ tcc_telas/lib/pages/saldo_page.dart
@@ -6,6 +6,9 @@ import 'home_page.dart';
 
 class saldoPage extends StatelessWidget {
   final _raController = TextEditingController();
+  final _senhaController = TextEditingController();
+  bool _raObrigatorio = false;
+  Color _colorButton = Color.fromARGB(255, 17, 122, 130);
 
   @override
   Widget build(BuildContext context) {
@@ -102,16 +105,92 @@ class saldoPage extends StatelessWidget {
                     color: Color(0xFFA12E2F),
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: SizedBox(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20, left: 15, right: 15),
                     child: Column(
                       children: [
-                        Padding(
-                          padding: EdgeInsets.all(16),
-                        ),
                         TextField(
-                          
-                        )
-                      ],
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey,
+                            hintText: 'RA',
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey,
+                            hintText: 'Senha',
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Align(
+                          alignment: Alignment.center,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                barrierDismissible: false,
+
+                                builder: (context) {
+                                  return AlertDialog(
+                                    backgroundColor: Color(0xFFA12E2F),
+                                    shape: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                                    title: Text(
+                                      'Saldo',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    content: Row(
+                                      children: [
+                                        Container(
+                                          width: 50,
+                                          margin: EdgeInsets.only(top: 15, left: 5),
+                                          child: Image.asset("image/carteira.png"),
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(top: 22, left: 5),
+                                          child: Text(
+                                            'O seu saldo é R\$111,00.',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    actions: [
+                                      ElevatedButton(
+                                        child: Text('OK'),
+                                        style: ElevatedButton.styleFrom(
+                                          primary: _colorButton,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      )
+                                    ],
+                                  );
+                                }
+                              );
+                            },
+                            child: Text('Consultar'),
+                            style: ElevatedButton.styleFrom(
+                              primary: _colorButton,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                            ),
+                          ),
+                      )
+                      ]
                     ),
                   )
                 ),

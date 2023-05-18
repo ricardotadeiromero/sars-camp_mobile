d01c9837b9fa96701ba761545955d277b83fda7b
chaba2005 <rtromero.sp@gmail.com>
2023-05-18 19:38:04 -0300

    arrumando cacas

--- tcc_telas/lib/pages/cardapio_page.dart
+++ tcc_telas/lib/pages/cardapio_page.dart
@@ -27,7 +27,7 @@ class _CardapioPage extends State<CardapioPage>
       return false;
   }
 
-  void defineFeriado()async {
+  void defineFeriado() async {
     verificaFeriado = [
       await checkFeriado(DateTime.monday),
       await checkFeriado(DateTime.tuesday),
@@ -36,7 +36,6 @@ class _CardapioPage extends State<CardapioPage>
       await checkFeriado(DateTime.friday),
     ];
   }
-
 
   @override
   void initState() {
@@ -47,143 +46,148 @@ class _CardapioPage extends State<CardapioPage>
       CardapioBodyPage(selectedDayWeek: DateTime.thursday),
       CardapioBodyPage(selectedDayWeek: DateTime.friday),
     ];
-    _controller = TabController(length: 5, vsync: this, initialIndex: DiaDaSemana.numberWeek());
+    _controller = TabController(
+        length: 5, vsync: this, initialIndex: DiaDaSemana.numberWeek());
     selectedDay = DateTime.now();
+    _indice = DiaDaSemana.numberWeek();
     defineFeriado();
     setDay();
     super.initState();
   }
 
-  void setDay() async{
-    if(await checkFeriado(DateTime.monday)){
-                        _controller.index = 1;
-                      }
-                      if(await checkFeriado(DateTime.tuesday)){
-                        _controller.index = 0;
-                      }
-                      if(await checkFeriado(DateTime.wednesday)){
-                        _controller.index = 1;
-                      }
-                      if(await checkFeriado(DateTime.thursday)){
-                        _controller.index = 2;
-                      }
-                      if(await checkFeriado(DateTime.friday)){
-                        _controller.index = 3;
-                      }
+  void setDay() async {
+    if (await checkFeriado(DateTime.monday)) {
+      _controller.index = 1;
+      _indice = 1;
+    }
+    if (await checkFeriado(DateTime.tuesday)) {
+      _controller.index = 0;
+      _indice = 0;
+    }
+    if (await checkFeriado(DateTime.wednesday)) {
+      _controller.index = 1;
+      _indice = 1;
+    }
+    if (await checkFeriado(DateTime.thursday)) {
+      _controller.index = 2;
+      _indice = 2;
+    }
+    if (await checkFeriado(DateTime.friday)) {
+      _controller.index = 3;
+      _indice = 3;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: MyAppBar(
-              shouldPopOnLogoPressed: true,
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(48),
-                child: TabBar(
-                  controller: _controller,
-                  isScrollable: false,
-                    indicatorColor: const Color.fromARGB(255, 15, 142, 147),
-                    tabs: [
-                      
-                      MyTab(date: DateTime.monday),
-                      MyTab(date: DateTime.tuesday),
-                      MyTab(date: DateTime.wednesday),
-                      MyTab(date: DateTime.thursday),
-                      MyTab(date: DateTime.friday),
-                    ],
-                    onTap: (index) {
-                      if(verificaFeriado[0]&&index==0){
-                        _controller.index = _controller.previousIndex;
-                        _indice = _controller.previousIndex;
-                      }
-                      if(verificaFeriado[1]&&index==1){
-                        _controller.index = _controller.previousIndex;
-                        _indice = _controller.previousIndex;
-                      }
-                      if(verificaFeriado[2]&&index==2){
-                        _controller.index = _controller.previousIndex;
-                        _indice = _controller.previousIndex;
-                      }
-                      if(verificaFeriado[3]&&index==3){
-                        _controller.index = _controller.previousIndex;
-                        _indice = _controller.previousIndex;
-                      }
-                      if(verificaFeriado[4]&&index==4){
-                        _controller.index = _controller.previousIndex;
-                        _indice = _controller.previousIndex;
-                      }
-                      setState(() {
-                        _indice = index;
-                      });
-                    }),
-              ),
-            ),
-            body: Background(
-              components: Container(
-                alignment: AlignmentDirectional.topStart,
-                child: SingleChildScrollView(
-                  child: bodyPage[_indice]
-                ),
-              ),
-            ));
+        appBar: MyAppBar(
+          shouldPopOnLogoPressed: true,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(48),
+            child: TabBar(
+                controller: _controller,
+                isScrollable: false,
+                indicatorColor: const Color.fromARGB(255, 15, 142, 147),
+                tabs: [
+                  MyTab(date: DateTime.monday),
+                  MyTab(date: DateTime.tuesday),
+                  MyTab(date: DateTime.wednesday),
+                  MyTab(date: DateTime.thursday),
+                  MyTab(date: DateTime.friday),
+                ],
+                onTap: (index) {
+                  if (verificaFeriado[0] && index == 0) {
+                    _controller.index = _controller.previousIndex;
+                    _indice = _controller.previousIndex;
+                  }
+                  if (verificaFeriado[1] && index == 1) {
+                    _controller.index = _controller.previousIndex;
+                    _indice = _controller.previousIndex;
+                  }
+                  if (verificaFeriado[2] && index == 2) {
+                    _controller.index = _controller.previousIndex;
+                    _indice = _controller.previousIndex;
+                  }
+                  if (verificaFeriado[3] && index == 3) {
+                    _controller.index = _controller.previousIndex;
+                    _indice = _controller.previousIndex;
+                  }
+                  if (verificaFeriado[4] && index == 4) {
+                    _controller.index = _controller.previousIndex;
+                    _indice = _controller.previousIndex;
+                  }
+                  setState(() {
+                    _indice = index;
+                  });
+                }),
+          ),
+        ),
+        body: Background(
+          components: Container(
+            alignment: AlignmentDirectional.topStart,
+            child: SingleChildScrollView(child: bodyPage[_indice]),
+          ),
+        ));
   }
 }
 
 class CardapioBodyPage extends StatelessWidget {
-   late Future<List<Cardapio>> future;
+  late Future<List<Cardapio>> future;
   late int selectedDayWeek;
 
-  CardapioBodyPage({Key? key, required this.selectedDayWeek}) : super(key: key) {
+  CardapioBodyPage({Key? key, required this.selectedDayWeek})
+      : super(key: key) {
     var selectedDay = DiaDaSemana.obterData(selectedDayWeek);
-    future = Connection.getCardapio(
-        DateFormat("yyyy-MM-dd").format(selectedDay));
+    future =
+        Connection.getCardapio(DateFormat("yyyy-MM-dd").format(selectedDay));
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-                    children: [
-                      ExpansionWidgetCafe(
-                          leadingIcon: const Icon(Icons.sunny_snowing),
-                          titleText: 'Café da manhã',
-                          future: MyListViewCafe()),
-                      ExpansionWidget(
-                          leadingIcon: const Icon(Icons.sunny),
-                          titleText: 'Almoço',
-                          subtitleText: 'Comum',
-                          future: CustomFutureBuilder<List<Cardapio>>(
-                            future: future,
-                            periodo: 0,
-                            vegetariano: 0,
-                          )),
-                      ExpansionWidget(
-                          leadingIcon: const Icon(Icons.sunny),
-                          titleText: 'Almoço',
-                          subtitleText: 'Vegetariano',
-                          future: CustomFutureBuilder<List<Cardapio>>(
-                            future: future,
-                            periodo: 0,
-                            vegetariano: 1,
-                          )),
-                      ExpansionWidget(
-                          leadingIcon: const Icon(Icons.nightlight),
-                          titleText: 'Jantar',
-                          subtitleText: 'Comum',
-                          future: CustomFutureBuilder<List<Cardapio>>(
-                            future: future,
-                            periodo: 1,
-                            vegetariano: 0,
-                          )),
-                      ExpansionWidget(
-                          leadingIcon: const Icon(Icons.nightlight),
-                          titleText: 'Jantar',
-                          subtitleText: 'Vegetariano',
-                          future: CustomFutureBuilder<List<Cardapio>>(
-                            future: future,
-                            periodo: 1,
-                            vegetariano: 1,
-                          )),
-                    ],
-                  );
+      children: [
+        ExpansionWidgetCafe(
+            leadingIcon: const Icon(Icons.sunny_snowing),
+            titleText: 'Café da manhã',
+            future: MyListViewCafe()),
+        ExpansionWidget(
+            leadingIcon: const Icon(Icons.sunny),
+            titleText: 'Almoço',
+            subtitleText: 'Comum',
+            future: CustomFutureBuilder<List<Cardapio>>(
+              future: future,
+              periodo: 0,
+              vegetariano: 0,
+            )),
+        ExpansionWidget(
+            leadingIcon: const Icon(Icons.sunny),
+            titleText: 'Almoço',
+            subtitleText: 'Vegetariano',
+            future: CustomFutureBuilder<List<Cardapio>>(
+              future: future,
+              periodo: 0,
+              vegetariano: 1,
+            )),
+        ExpansionWidget(
+            leadingIcon: const Icon(Icons.nightlight),
+            titleText: 'Jantar',
+            subtitleText: 'Comum',
+            future: CustomFutureBuilder<List<Cardapio>>(
+              future: future,
+              periodo: 1,
+              vegetariano: 0,
+            )),
+        ExpansionWidget(
+            leadingIcon: const Icon(Icons.nightlight),
+            titleText: 'Jantar',
+            subtitleText: 'Vegetariano',
+            future: CustomFutureBuilder<List<Cardapio>>(
+              future: future,
+              periodo: 1,
+              vegetariano: 1,
+            )),
+      ],
+    );
   }
 }
